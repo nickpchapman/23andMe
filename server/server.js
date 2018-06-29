54570f0ec9422cb8157c00f19e930201e4fef3d9
Nick Chapman <nickpchapman@gmail.com>
2018-06-29 15:59:49 -0700

    add axios for API calls, add accession and marker endpoints to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,46 @@
+const axios = require("axios");
+
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+//default endpoint
 app.get("/", (req, res) => {
-  res.send("booya!");
+  res.send("23andMe API at your service. Try: /accession, /marker");
+});
+
+//will return base count of chromosome
+// "https://api.23andme.com/3/accession/?chromosome=1"
+//curl "https://api.23andme.com/3/accession/NC_000001.10"
+app.get("/accession", (req, res) => {
+  axios
+    .get("https://api.23andme.com/3/accession/NC_012920.1")
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+// will return a list of SNP markers for that gene(not full list in gene, must be list of snps used by 23andMe...): "https://api.23andme.com/3/marker/?gene_name=ACTN3"
+
+// will return a list of markers that match those ids: curl "https://api.23andme.com/3/marker/?id=rs10195681,i4001358"
+//  will return a list of markers on that chromosome: curl "https://api.23andme.com/3/marker/?accession_id=NC_012920.1"
+app.get("/marker", (req, res) => {
+  axios
+    .get("https://api.23andme.com/3/marker/?gene_name=TP53")
+    .then(response => {
+      console.log(
+        "number of snps in this list =======> ",
+        response.data.data.length
+      );
+      res.json(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
 
 const server = app.listen(port, () => {
